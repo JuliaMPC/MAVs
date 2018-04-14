34d25329f290c712e8bff9e9246594242c45cf1c
Sumit Joshi <jsumit@umich.edu>
2018-04-14 15:58:05 -0400

    Fixed demo D after updates to chrono

--- ros/src/computing/planning/motion/mavs_ros_planner/src/NavsPathConverter.cpp
+++ ros/src/computing/planning/motion/mavs_ros_planner/src/NavsPathConverter.cpp
@@ -6,21 +6,22 @@
 std::string planner_ns;
 
 void poseCallback(const nav_msgs::Path path) {
-  std::vector<float> x_p;
-  std::vector<float> y_p;
+  std::vector<double> x_p;
+  std::vector<double> y_p;
    for (int i = 0; i < path.poses.size(); i++) {
-     x_p.push_back(roundf(path.poses[i].pose.position.x * 100)/100.0);
-     y_p.push_back(roundf(path.poses[i].pose.position.y * 100)/100.0);
+     x_p.push_back(round(path.poses[i].pose.position.x * 100)/100.0);
+     y_p.push_back(round(path.poses[i].pose.position.y * 100)/100.0);
      //i++; // skipping one point tro reduce resolution
    }
 
    if (x_p.size() >= 2 && y_p.size() >= 2) {
 
-     ros::param::set(planner_ns + "/traj/" + "x", x_p);
-     ros::param::set(planner_ns + "/traj/" + "yVal", y_p);
+     ros::param::set("state/chrono/"+planner_ns + "/traj/" + "x", x_p);
+     ros::param::set("state/chrono/"+planner_ns + "/traj/" + "yVal", y_p);
 
      ros::param::set("/system/" + planner_ns + "/flags/initialized", true);
    }
+
 }
 
 
@@ -32,9 +33,8 @@ int main(int argc, char** argv) {
 
   ros::param::get("system/planner", planner_ns);
 
-  ros::Subscriber sub = n.subscribe<nav_msgs::Path>("/move_base/NavfnROS/plan", 1000, poseCallback);
-  //n.setParam("/system/" + planner_ns + "/flags/path_converter_initialized", true);
-  //ros::spin();
+  ros::Subscriber sub = n.subscribe<nav_msgs::Path>("/move_base/TrajectoryPlannerROS/global_plan", 1000, poseCallback);
+  //ros::Subscriber sub = n.subscribe<nav_msgs::Path>("/move_base/NavfnROS/plan", 1000, poseCallback);
   while (1)
   {
     ros::spinOnce();
