4467a13fa0a1f05a053eed35e83b9a7de5eb55ea
ChangYuHsuan <changyh1994@gmail.com>
2018-10-16 19:54:45 -0400

    Create volume for sharing source files in src/

--- run.sh
+++ run.sh
@@ -13,14 +13,18 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/home/$USER/.Xauthority
 SHARED_DIR=/home/mavs/MAVs/shared_dir
 HOST_DIR=/home/$USER/MAVs/shared_dir
+SRC_CONTAINER=/home/mavs/MAVs/ros/src
+SRC_HOST=/home/$USER/MAVs/ros/src
 
 echo "Shared directory: ${HOST_DIR}"
 
 nvidia-docker run \
+    --user mavs \
     -it --rm \
     --volume=$XSOCK:$XSOCK:rw \
     --volume=$XAUTH:$XAUTH:rw \
     --volume=$HOST_DIR:$SHARED_DIR:rw \
+    --volume=$SRC_HOST:$SRC_CONTAINER:rw \
     --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY=${DISPLAY}" \
     --privileged -v /dev/bus/usb:/dev/bus/usb \
