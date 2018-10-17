3d8e22cbc7820aa48e8e0001c08daaff8c9392bd
ChangYuHsuan <changyh1994@gmail.com>
2018-10-17 15:24:07 -0400

    use  command to set the volume host directory

--- run.sh
+++ run.sh
@@ -11,19 +11,19 @@ done
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/home/$USER/.Xauthority
-SHARED_DIR=/home/mavs/MAVs/shared_dir
-HOST_DIR=/home/$USER/MAVs/shared_dir
+SHARED_CONTAINER=/home/mavs/MAVs/shared_dir
+SHARED_HOST="$(pwd)"/shared_dir
 SRC_CONTAINER=/home/mavs/MAVs/ros/src
-SRC_HOST=/home/$USER/MAVs/ros/src
+SRC_HOST="$(pwd)"/ros/src
 
-echo "Shared directory: ${HOST_DIR}"
+echo "Shared directory: ${SHARED_CONTAINER}"
 
 nvidia-docker run \
     --user mavs \
     -it --rm \
     --volume=$XSOCK:$XSOCK:rw \
     --volume=$XAUTH:$XAUTH:rw \
-    --volume=$HOST_DIR:$SHARED_DIR:rw \
+    --volume=$SHARED_HOST:$SHARED_CONTAINER:rw \
     --volume=$SRC_HOST:$SRC_CONTAINER:rw \
     --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY=${DISPLAY}" \
