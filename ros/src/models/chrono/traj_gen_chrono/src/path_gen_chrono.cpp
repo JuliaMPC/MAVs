de22e7968a45e3403f4498a8af88ee195dd7bb20
Michael <qingjqi@umich.edu>
2018-11-03 23:58:57 -0400

    improved the algorithm of path_gen_chrono. It now starts the vehicle faster and controls the switching of path based on current path

--- ros/src/models/chrono/traj_gen_chrono/src/path_gen_chrono.cpp
+++ ros/src/models/chrono/traj_gen_chrono/src/path_gen_chrono.cpp
@@ -12,18 +12,17 @@
 
 using namespace chrono;
 
-double veh_x_pos;
-double veh_y_pos;
+double veh_pos_x;
+double veh_pos_y;
 double veh_v;
 
 void vehCallback(const ros_chrono_msgs::veh_status::ConstPtr& veh_msgs) {
     veh_v = std::sqrt(pow(veh_msgs->x_v, 2) + pow(veh_msgs->y_v, 2));
-    veh_x_pos = veh_msgs->x_pos;
-    veh_y_pos = veh_msgs->y_pos;
+    veh_pos_x = veh_msgs->x_pos;
+    veh_pos_y = veh_msgs->y_pos;
 }
 
 int main(int argc, char **argv) {
-    
     // initialize rosnode
     ros::init(argc, argv, "Reference");
     // create node handle
@@ -42,28 +41,39 @@ int main(int argc, char **argv) {
     node.getParam("case/path/path_2/x", path_2_x);
     node.getParam("case/path/path_2/y", path_2_y);
 
-    double switch_rate, control_vx;
-    node.getParam("case/path/switch_rate", switch_rate);
+    double test_rate, control_vx, vel_tol, pos_tol, pos_delay;
+    node.getParam("case/path/test_rate", test_rate);
     node.getParam("case/goal/v", control_vx);
+    node.getParam("case/path/vel_tol", vel_tol);
+    node.getParam("case/path/pos_tol", pos_tol);
+    node.getParam("case/path/pos_delay", pos_delay);
 
     nloptcontrol_planner::Control control_info;
     control_info.vx = std::vector<double> {control_vx};
-    ros::Rate loop_rate(switch_rate);
+    ros::Rate loop_rate(test_rate);
 
-    int count = 0;
+    int count = 0, path_status = 0;
     while (ros::ok()) {
-        count++;
-        if (count % 2){
-            ROS_INFO("Swithed to first lane");
-            control_info.x =  path_1_x;
-            control_info.y =  path_1_y;
-        }
-        else {
-            ROS_INFO("Swithed to second lane");
-            control_info.x =  path_2_x;
-            control_info.y =  path_2_y;
+
+        if (fabs(control_vx - veh_v) < vel_tol) {
+            if (fabs(veh_pos_y - path_2_y[0]) < pos_tol) {
+                if (count++ >= pos_delay*test_rate) {
+                    ROS_INFO("Swithed to first lane");
+                    path_status = 1;
+                    count = 0;
+                }
+            }
+            else if(fabs(veh_pos_y - path_1_y[0]) < pos_tol) {
+                if (count++ >= pos_delay*test_rate) {
+                    ROS_INFO("Swithed to second lane");
+                    path_status = 0;
+                    count = 0;
+                }
+            }
         }
 
+        control_info.x = path_status ? path_1_x : path_2_x;
+        control_info.y = path_status ? path_1_y : path_2_y;
         pub.publish(control_info);
 
         ros::spinOnce();
