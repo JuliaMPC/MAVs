74d218c5592b117b043ea9b22043166c141e3b3c
Huckleberry Febbo <febbo@umich.edu>
2018-11-18 19:11:36 -0500

    started to fix chrono and gazebo sycronization issue

--- ros/src/models/chrono/sim_time/src/ros_get_world_time.cpp
+++ ros/src/models/chrono/sim_time/src/ros_get_world_time.cpp
@@ -1,3 +1,7 @@
+// http://answers.gazebosim.org/question/12138/perform-one-simulation-step/
+// https://bitbucket.org/osrf/gazebo/src/5299916de5cce4a5f3baf8acf75cf6bff68f8184/gazebo/msgs/world_control.proto?at=default&fileviewer=file-view-default
+// http://answers.gazebosim.org/question/18995/gazebo-simulated-time-control-step-by-step/
+//https://github.com/jaredmoore/ROS-Gazebo-Examples/blob/master/gazebo_step_world/world_step.cc
 #include <math.h>
 
 #include <boost/thread.hpp>
@@ -26,21 +30,21 @@ int main(int argc, char** argv)
   gazebo::transport::PublisherPtr pub = node->Advertise<gazebo::msgs::WorldControl>("~/world_control");
 
   // Waits for simulation time update.
-  ros::Time last_ros_time_;
-  bool wait = true;
-  double simulation_time_pre;
-  rosnode->getParam("state/t", simulation_time_pre);
-  double simulation_time = simulation_time_pre;
-  double delta_time = 0;
-  
+//  ros::Time last_ros_time_;
+
+//  double simulation_time_pre;
+  double simulation_time;
+  rosnode->getParam("state/t", simulation_time);
+//  double delta_time = 0;
+
   bool is_init;
   rosnode->setParam("system/sim_time/flags/initialized", true);
   rosnode->getParam("system/flags/initialized", is_init);
   while (!is_init) {
       rosnode->getParam("system/flags/initialized", is_init);
   }
-
-  while (wait)
+  double ros_time;
+  while (1)
   {
     // last_ros_time_ = ros::Time::now();
     // std::cout << "\t\t\t Attempted getting sim time: " << last_ros_time_ << std::endl;
@@ -50,16 +54,32 @@ int main(int argc, char** argv)
 
     // Publish the step message for the simulation.
     gazebo::msgs::WorldControl msg;
-    msg.set_step(true);
 
+    // get Gazebo time and if it is less than Chrono time then step, else pause
     // get simulation step
     rosnode->getParam("state/t", simulation_time);
 
-    delta_time = simulation_time - simulation_time_pre;
-    if(delta_time)  pub->Publish(msg);
-    // Wait for 1 second and allow ROS to complete as well.
-    gazebo::common::Time::MSleep(delta_time*100.0);
-    simulation_time_pre = simulation_time;
+    //delta_time = simulation_time - simulation_time_pre;
+    ros_time = ros::Time::now().toSec();
+    //if(delta_time)  pub->Publish(msg);
+    //pub->Publish(msg);
+    if(ros_time < simulation_time){
+      msg.set_pause(0);
+      msg.set_step(1);
+      pub->Publish(msg);
+      ROS_INFO("STEP");
+    }
+    else{
+      msg.set_pause(1);
+      pub->Publish(msg);
+      ROS_INFO("PAUSE");
+    }
+    std :: cout << "chrono: " << simulation_time << " , ros: " << ros_time << std::endl;
+
+    // Wait for 1 msecond and allow ROS to complete as well.
+
+  //  gazebo::common::Time::MSleep(delta_time*100.0);
+  //  simulation_time_pre = simulation_time;
     // std :: cout << "Time in seconds: " << simulation_time << " Delta_time: " << delta_time << std::endl;
     // ros::spinOnce();
   }
