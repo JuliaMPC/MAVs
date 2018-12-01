76fab00610e755e4473321e21a8bab2b3bfd13ae
xingjianliu <liuxingj@umich.edu>
2018-11-30 21:55:47 -0500

    Add the past planned trajectory and solved time

--- ros/src/computing/tools/visualization/data_plotter.py
+++ ros/src/computing/tools/visualization/data_plotter.py
@@ -13,32 +13,42 @@ class DataPlotter():
         # time data
         self.t_actual = np.array([], dtype=np.float64)
         self.t_traj = np.array([], dtype=np.float64)
+        self.t_traj_last = np.array([], dtype=np.float64)
         
         # position data
         self.x_actual = np.array([], dtype=np.float64)
         self.y_actual = np.array([], dtype=np.float64)
         self.x_traj = np.array([], dtype=np.float64)
         self.y_traj = np.array([], dtype=np.float64)
+        self.x_traj_last = np.array([], dtype=np.float64)
+        self.y_traj_last = np.array([], dtype=np.float64)
 
         # steering angle data
         self.sa_actual = np.array([], dtype=np.float64)
         self.sa_traj = np.array([], dtype=np.float64)
+        self.sa_traj_last = np.array([], dtype=np.float64)
         self.sa_upper_limit = np.array([], dtype=np.float64)
         self.sa_lower_limit = np.array([], dtype=np.float64)
 
         # longitudinal velocity data
         self.ux_actual = np.array([], dtype=np.float64)
         self.ux_traj = np.array([], dtype=np.float64)
+        self.ux_traj_last = np.array([], dtype=np.float64)
 
         # longitudinal acceleration data
         self.ax_actual = np.array([], dtype=np.float64)
         self.ax_traj = np.array([], dtype=np.float64)
+        self.ax_traj_last = np.array([], dtype=np.float64)
         self.ax_upper_limit = np.array([], dtype=np.float64)
         self.ax_lower_limit = np.array([], dtype=np.float64)
 
         # solving time data
         self.solve_num = np.array([], dtype=np.uint32)
+        self.solve_num_pass = np.array([], dtype=np.uint32)
+        self.solve_num_fail = np.array([], dtype=np.uint32)
         self.solve_time = np.array([], dtype=np.float64)
+        self.solve_time_pass = np.array([], dtype=np.float64)
+        self.solve_time_fail = np.array([], dtype=np.float64)
         self.solve_time_limit = np.array([], dtype=np.float64)
 
         # setup matplot figure
@@ -61,9 +71,11 @@ class DataPlotter():
 
         self.line_pos_actual = Line2D([], [], color='green', linewidth=1.5, linestyle='-')
         self.line_pos_traj = Line2D([], [], color='red', linewidth=1, linestyle='--')
+        self.line_pos_traj_last = Line2D([], [], color='orange', linewidth=1, linestyle='--')
         self.pos_subplot.add_line(self.line_pos_actual)
         self.pos_subplot.add_line(self.line_pos_traj)
-        self.pos_subplot.legend(['actual path', 'planned path'], fontsize='x-small')
+        self.pos_subplot.add_line(self.line_pos_traj_last)
+        self.pos_subplot.legend(['actual path', 'planned path', 'last planned path'], fontsize='x-small')
         self.pos_subplot.set_xlabel('x (m)')
         self.pos_subplot.set_ylabel('y (m)')
         self.pos_subplot.tick_params('both', labelsize='small')
@@ -74,9 +86,11 @@ class DataPlotter():
 
         self.line_sa_actual = Line2D([], [], color='green', linewidth=2, linestyle='-')
         self.line_sa_traj = Line2D([], [], color='red', linewidth=1.5, linestyle='--')
+        self.line_sa_traj_last = Line2D([], [], color='orange', linewidth=1.5, linestyle='--')
         self.sa_subplot.add_line(self.line_sa_actual)
         self.sa_subplot.add_line(self.line_sa_traj)
-        self.sa_subplot.legend(['actual sa', 'planned sa'], fontsize='x-small')
+        self.sa_subplot.add_line(self.line_sa_traj_last)
+        self.sa_subplot.legend(['actual sa', 'planned sa', 'last planned sa'], fontsize='x-small')
         self.sa_subplot.set_xlabel('time (s)', fontsize='small', verticalalignment='center')
         self.sa_subplot.set_ylabel('steering angle (rad)', fontsize='small')
         self.sa_subplot.tick_params('both', labelsize='x-small')
@@ -86,9 +100,11 @@ class DataPlotter():
 
         self.line_ux_actual = Line2D([], [], color='green', linewidth=2, linestyle='-')
         self.line_ux_traj = Line2D([], [], color='red', linewidth=1.5, linestyle='--')
+        self.line_ux_traj_last = Line2D([], [], color='orange', linewidth=1.5, linestyle='--')
         self.ux_subplot.add_line(self.line_ux_actual)
         self.ux_subplot.add_line(self.line_ux_traj)
-        self.ux_subplot.legend(['actual ux', 'planned ux'], fontsize='x-small')
+        self.ux_subplot.add_line(self.line_ux_traj_last)
+        self.ux_subplot.legend(['actual ux', 'planned ux', 'last planned ux'], fontsize='x-small')
         self.ux_subplot.set_xlabel('time (s)', fontsize='small', verticalalignment='center')
         self.ux_subplot.set_ylabel('longitudinal velocity (m/s)', fontsize='small')
         self.ux_subplot.tick_params('both', labelsize='x-small')
@@ -98,9 +114,11 @@ class DataPlotter():
 
         self.line_ax_actual = Line2D([], [], color='green', linestyle='-')
         self.line_ax_traj = Line2D([], [], color='red', linewidth=1.5, linestyle='--')
+        self.line_ax_traj_last = Line2D([], [], color='orange', linewidth=1.5, linestyle='--')
         self.ax_subplot.add_line(self.line_ax_actual)
         self.ax_subplot.add_line(self.line_ax_traj)
-        self.ax_subplot.legend(['actual ax', 'planned ax'], fontsize='x-small')
+        self.ax_subplot.add_line(self.line_ax_traj_last)
+        self.ax_subplot.legend(['actual ax', 'planned ax', 'last planned ax'], fontsize='x-small')
         self.ax_subplot.set_xlabel('time (s)', fontsize='small', verticalalignment='center')
         self.ax_subplot.set_ylabel('longitudinal acceleration (m/s)', fontsize='small')
         self.ax_subplot.tick_params('both', labelsize='x-small')
@@ -109,8 +127,12 @@ class DataPlotter():
         self.ax_subplot.set_ylim(-self.ax_band/2.0,self.ax_band/2.0)
 
         self.line_solve_time = Line2D([], [], marker='*', color='green', linestyle='')
+        self.line_solve_time_pass = Line2D([], [], marker='*', color='green', linestyle='')
+        self.line_solve_time_fail = Line2D([], [], marker='*', color='red', linestyle='')
         self.line_solve_time_limit = Line2D([], [], color='red', linewidth=1.5, linestyle='--')
-        self.tSolve_subplot.add_line(self.line_solve_time)
+        # self.tSolve_subplot.add_line(self.line_solve_time)
+        self.tSolve_subplot.add_line(self.line_solve_time_pass)
+        self.tSolve_subplot.add_line(self.line_solve_time_fail)
         self.tSolve_subplot.add_line(self.line_solve_time_limit)
         self.tSolve_subplot.set_xlabel('number of optimization calculations', fontsize='small', verticalalignment='center')
         self.tSolve_subplot.set_ylabel('solve time (s)', fontsize='small')
@@ -124,22 +146,28 @@ class DataPlotter():
 
             self.line_pos_actual.set_data(self.x_actual, self.y_actual)
             self.line_pos_traj.set_data(self.x_traj, self.y_traj)
+            self.line_pos_traj_last.set_data(self.x_traj_last, self.y_traj_last)
             if (len(self.x_actual) and len(self.y_actual)):
                 self.pos_subplot.set_xlim(self.x_actual[-1]-self.x_band/2, self.x_actual[-1]+self.x_band/2)
                 self.pos_subplot.set_ylim(self.y_actual[-1]-self.y_band/2, self.y_actual[-1]+self.y_band/2)
 
             self.line_sa_actual.set_data(self.t_actual, self.sa_actual)
             self.line_sa_traj.set_data(self.t_traj, self.sa_traj)
+            self.line_sa_traj_last.set_data(self.t_traj_last, self.sa_traj_last)
 
             self.line_ux_actual.set_data(self.t_actual, self.ux_actual)
             self.line_ux_traj.set_data(self.t_traj, self.ux_traj)
+            self.line_ux_traj_last.set_data(self.t_traj_last, self.ux_traj_last)
             if (len(self.ux_actual)):
                 self.ux_subplot.set_ylim(self.ux_actual[-1]-self.ux_band/2, self.ux_actual[-1]+self.ux_band/2)
 
             self.line_ax_actual.set_data(self.t_actual, self.ax_actual)
             self.line_ax_traj.set_data(self.t_traj, self.ax_traj)
+            self.line_ax_traj_last.set_data(self.t_traj_last, self.ax_traj_last)
 
             self.line_solve_time.set_data(self.solve_num, self.solve_time)
+            self.line_solve_time_pass.set_data(self.solve_num_pass, self.solve_time_pass)
+            self.line_solve_time_fail.set_data(self.solve_num_fail, self.solve_time_fail)
             self.line_solve_time_limit.set_data(self.solve_num, self.solve_time_limit)
 
             # moving time axis
@@ -170,6 +198,13 @@ def stateCallback(msg):
 def trajectoryCallback(msg):
     global data_plotter
 
+    data_plotter.t_traj_last = data_plotter.t_traj
+    data_plotter.x_traj_last = data_plotter.x_traj
+    data_plotter.y_traj_last = data_plotter.y_traj
+    data_plotter.sa_traj_last = data_plotter.sa_traj
+    data_plotter.ux_traj_last = data_plotter.ux_traj
+    data_plotter.ax_traj_last = data_plotter.ax_traj
+
     data_plotter.t_traj = msg.t
     data_plotter.x_traj = msg.x
     data_plotter.y_traj = msg.y
@@ -180,6 +215,12 @@ def trajectoryCallback(msg):
 def optCallback(msg):
     global data_plotter
 
+    if (msg.tSolve < 0.5) & (msg.status == 'Optimal') :
+        data_plotter.solve_time_pass = np.append(data_plotter.solve_time_pass, msg.tSolve)
+        data_plotter.solve_num_pass = np.append(data_plotter.solve_num_pass, len(data_plotter.solve_num))
+    else:
+        data_plotter.solve_time_fail = np.append(data_plotter.solve_time_fail, msg.tSolve)
+        data_plotter.solve_num_fail = np.append(data_plotter.solve_num_fail, len(data_plotter.solve_num))
     data_plotter.solve_time = np.append(data_plotter.solve_time, msg.tSolve)
     data_plotter.solve_time_limit = np.append(data_plotter.solve_time_limit, 0.5)
     data_plotter.solve_num = np.append(data_plotter.solve_num, len(data_plotter.solve_time))
