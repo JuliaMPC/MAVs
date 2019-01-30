40dcbe718b8b1fafd80b5a676ede88b01cb27927
Huckleberry Febbo <febbo@umich.edu>
2019-01-30 14:34:17 -0500

    changed the name of sweepA to sweep and made it so that a folder needs to be passed for the results

--- ros/src/system/param_sweep/scripts/sweep.sh
+++ ros/src/system/param_sweep/scripts/sweep.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# $0 is the script name, $1 id is the folder name
+
+FOLDERNAME="$1"
+rm -rf /home/mavs/MAVs/results/$FOLDERNAME/*
+rmdir /home/mavs/MAVs/results/$FOLDERNAME
+mkdir /home/mavs/MAVs/results/$FOLDERNAME
+
 wait_for_roscore_to_initialize () {
   echo "Entering: wait_for_roscore_to_initialize"
   until rostopic list ; do sleep 1; done
@@ -55,7 +62,7 @@ start_roscore () {
 run_launchfile () {
   echo "Entering: run_launchfile"
   update_dynamic_ros_parameters
-  roslaunch system sweepA.launch &
+  roslaunch system $FOLDERNAME.launch &
   roslaunch_PID=$!
   sleep 4
   echo "Exiting: run_launchfile"
@@ -92,6 +99,7 @@ loop_entry_point () {
 }
 
 planners=( "true" "false" )
+unknowns=( "true" "false")
 #vys=([-10.,0.,0.,-1.] [-9.5,0.,0.,-1.] [-9.,0.,0.,-1.] [-8.5,0.,0.,-1.] [-8.,0.,0.,-1.] [-7.5,0.,0.,-1.] [-7.,0.,0.,-1.] [-6.5,0.,0.,-1.] [-6.,0.,0.,-1.] [-5.,0.,0.,-1.] [-5.5,0.,0.,-1.] [-5.,0.,0.,-1.] [-4.5,0.,0.,-1.] [-4.,0.,0.,-1.] [-3.5,0.,0.,-1.] [-3.,0.,0.,-1.] [-2.5,0.,0.,-1.] [-2.,0.,0.,-1.] [-1.5,0.,0.,-1.] [-1.,0.,0.,-1.] [-0.5,0.,0.,-1.] [0.,0.,0.,-1.])
 #radi=( [0.25,10.,5.,12.] [0.5,10.,5.,12.] [1.,10.,5.,12.] [1.5,10.,5.,12.] [2.,10.,5.,12.] [2.5,10.,5.,12.] [3.,10.,5.,12.] [3.5,10.,5.,12.] [4.,10.,5.,12.] [4.5,10.,5.,12.] [5.,10.,5.,12.] [5.5,10.,5.,12.] [6.5,10.,5.,12.] [7.5,10.,5.,12.] [8.,10.,5.,12.] [8.5,10.,5.,12.] [9.,10.,5.,12.] [9.5,10.,5.,12.] [10.,10.,5.,12.] )
 vys=( [-20.,0.,0.,-1.] [-19.,0.,0.,-1.] [-18,0.,0.,-1.] [-17.,0.,0.,-1.] [-16.,0.,0.,-1.] [-15.,0.,0.,-1.] [-14.,0.,0.,-1.] [-13.,0.,0.,-1.] [-12.,0.,0.,-1.] [-11,0.,0.,-1.] [-10.,0.,0.,-1.] [-9,0.,0.,-1.] [-8.,0.,0.,-1.] [-7,0.,0.,-1.] [-6.,0.,0.,-1.] [-5,0.,0.,-1.] [-4.,0.,0.,-1.] [-3,0.,0.,-1.] [-2.,0.,0.,-1.] [-1,0.,0.,-1.] [0.,0.,0.,-1.])
@@ -114,13 +122,13 @@ for radius in ${radi[@]}; do
       rosparam set "/case/id" test$idx
       loop_entry_point
       echo "Entering: postProcess"
-      mkdir /home/mavs/MAVs/results/sweepA/test"$idx"
+      mkdir /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"
       cd /home/mavs/MAVs/results;
-      python bag_to_csv.py tmp.bag "sweepA/test$idx"
-      julia plottingData.jl "sweepA/test$idx" "tmp"
-      rm /home/mavs/MAVs/results/sweepA/test$idx/control.csv
-      rm /home/mavs/MAVs/results/sweepA/test$idx/state.csv
-      rm /home/mavs/MAVs/results/sweepA/test$idx/nloptcontrol_plannercontrol.csv
+      python bag_to_csv.py tmp.bag "$FOLDERNAME/test$idx"
+      julia plottingData.jl "$FOLDERNAME/test$idx" "tmp"
+      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/control.csv
+      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/state.csv
+      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/nloptcontrol_plannercontrol.csv
       echo "Exiting: postProcess"
       idx=$(( $idx+1 ))
     done
