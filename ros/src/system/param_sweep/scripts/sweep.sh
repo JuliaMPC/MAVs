5651d29d40e83aa0b5106bdc7066227a8f5a1326
Huckleberry Febbo <febbo@umich.edu>
2019-01-31 01:05:53 -0500

    added an unknown environment option in loop

--- ros/src/system/param_sweep/scripts/sweep.sh
+++ ros/src/system/param_sweep/scripts/sweep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $0 is the script name, $1 id is the folder name
+# $0 is the script name, $1 is the folder name
 
 FOLDERNAME="$1"
 rm -rf /home/mavs/MAVs/results/$FOLDERNAME/*
@@ -38,13 +38,16 @@ update_dynamic_ros_parameters () {
     rosparam set $key ${parameters[$key]}
   done
   # show where in the loop it is
+  echo `rosparam get "/system/nloptcontrol_planner/flags/known_environment"`
   echo `rosparam get "/planner/nloptcontrol_planner/misc/movingObstacles"`
   echo `rosparam get "/case/actual/obstacle/vy"`
   echo `rosparam get "/case/actual/obstacle/radius"`
+
   # remove so that the script fails if they are not created
   rm /home/mavs/MAVs/ros/src/system/config/case/tmp.yaml
   rm /home/mavs/MAVs/ros/src/system/config/planner/nloptcontrol_planner/tmp.yaml
-  # create temp files
+  # create temp YAML files that can be loaded by the functions: obstacle_avoidance.jl and in the launch file (to programatically create obstacles)
+  # while these parameters are currently on the ros server, these functions that need to load these directly from a file
   cd /home/mavs/MAVs/ros/src/system/param_sweep/scripts
   sh ./rosparam_append.sh /home/mavs/MAVs/ros/src/system/config/case/s8Sweep.yaml / /home/mavs/MAVs/ros/src/system/config/case/tmp.yaml
   sh ./rosparam_append.sh /home/mavs/MAVs/ros/src/system/config/planner/nloptcontrol_planner/Dsweep.yaml / /home/mavs/MAVs/ros/src/system/config/planner/nloptcontrol_planner/tmp.yaml
@@ -76,13 +79,15 @@ until (($(( SECONDS - START_TIME )) > "$DURATION")) || [[ $shutdown -eq 1 ]]; do
   sleep 1;
   if [[ `rosparam get "system/flags/done"` = "true" ]]; then
     echo "system/flags/done = true"
+    sleep 20;  # assuming that this is enough time, but there is not check to make sure that everything has shutdown.
     shutdown=1;
-    sleep 45;  # assuming that this is enough time, but there is not check to make sure that everything has shutdown.
+    sleep 20;
   fi
   if [[ `rosparam get "/vehicle_collided"` = "true" ]]; then
     echo "/vehicle_collided = true"
+    sleep 20;  # assuming that this is enough time, but there is not check to make sure that everything has shutdown.
     shutdown=1;
-    sleep 45;  # assuming that this is enough time, but there is not check to make sure that everything has shutdown.
+    sleep 20;
   fi
 done
 sleep 10;
@@ -99,7 +104,7 @@ loop_entry_point () {
 }
 
 planners=( "true" "false" )
-unknowns=( "true" "false")
+knowns=( "true" "false")
 #vys=([-10.,0.,0.,-1.] [-9.5,0.,0.,-1.] [-9.,0.,0.,-1.] [-8.5,0.,0.,-1.] [-8.,0.,0.,-1.] [-7.5,0.,0.,-1.] [-7.,0.,0.,-1.] [-6.5,0.,0.,-1.] [-6.,0.,0.,-1.] [-5.,0.,0.,-1.] [-5.5,0.,0.,-1.] [-5.,0.,0.,-1.] [-4.5,0.,0.,-1.] [-4.,0.,0.,-1.] [-3.5,0.,0.,-1.] [-3.,0.,0.,-1.] [-2.5,0.,0.,-1.] [-2.,0.,0.,-1.] [-1.5,0.,0.,-1.] [-1.,0.,0.,-1.] [-0.5,0.,0.,-1.] [0.,0.,0.,-1.])
 #radi=( [0.25,10.,5.,12.] [0.5,10.,5.,12.] [1.,10.,5.,12.] [1.5,10.,5.,12.] [2.,10.,5.,12.] [2.5,10.,5.,12.] [3.,10.,5.,12.] [3.5,10.,5.,12.] [4.,10.,5.,12.] [4.5,10.,5.,12.] [5.,10.,5.,12.] [5.5,10.,5.,12.] [6.5,10.,5.,12.] [7.5,10.,5.,12.] [8.,10.,5.,12.] [8.5,10.,5.,12.] [9.,10.,5.,12.] [9.5,10.,5.,12.] [10.,10.,5.,12.] )
 vys=( [-20.,0.,0.,-1.] [-19.,0.,0.,-1.] [-18,0.,0.,-1.] [-17.,0.,0.,-1.] [-16.,0.,0.,-1.] [-15.,0.,0.,-1.] [-14.,0.,0.,-1.] [-13.,0.,0.,-1.] [-12.,0.,0.,-1.] [-11,0.,0.,-1.] [-10.,0.,0.,-1.] [-9,0.,0.,-1.] [-8.,0.,0.,-1.] [-7,0.,0.,-1.] [-6.,0.,0.,-1.] [-5,0.,0.,-1.] [-4.,0.,0.,-1.] [-3,0.,0.,-1.] [-2.,0.,0.,-1.] [-1,0.,0.,-1.] [0.,0.,0.,-1.])
@@ -111,26 +116,37 @@ idx=1;
 for radius in ${radi[@]}; do
    for vy in ${vys[@]}; do
      for plan in ${planners[@]}; do
-      echo "______________________________________"
-      echo "Running for the $idx nd time."
-      echo "--------------------------------------"
-      parameters["/planner/nloptcontrol_planner/misc/movingObstacles"]=${plan}
-      parameters["/case/actual/obstacle/vy"]=${vy}
-      parameters["/case/actual/obstacle/radius"]=${radius}
-      start_roscore
-      sleep 2;
-      rosparam set "/case/id" test$idx
-      loop_entry_point
-      echo "Entering: postProcess"
-      mkdir /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"
-      cd /home/mavs/MAVs/results;
-      python bag_to_csv.py tmp.bag "$FOLDERNAME/test$idx"
-      julia plottingData.jl "$FOLDERNAME/test$idx" "tmp"
-      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/control.csv
-      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/state.csv
-      rm /home/mavs/MAVs/results/$FOLDERNAME/test$idx/nloptcontrol_plannercontrol.csv
-      echo "Exiting: postProcess"
-      idx=$(( $idx+1 ))
+       for known in ${knowns[@]}; do
+        echo "______________________________________"
+        echo "Running for the $idx th time."
+        echo "--------------------------------------"
+        parameters["/system/nloptcontrol_planner/flags/known_environment"]=${known}
+        parameters["/planner/nloptcontrol_planner/misc/movingObstacles"]=${plan}
+        parameters["/case/actual/obstacle/vy"]=${vy}
+        parameters["/case/actual/obstacle/radius"]=${radius}
+        start_roscore
+        sleep 2;
+        rosparam set "/case/id" test"$idx"
+        loop_entry_point
+        echo "Entering: postProcess"
+        mkdir /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"
+        cd /home/mavs/MAVs/results
+        python bag_to_csv.py tmp.bag "$FOLDERNAME/test"$idx""
+        cd /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"
+        ls
+        cd /home/mavs/MAVs/results
+        julia plottingData.jl "$FOLDERNAME/test"$idx"" "tmp"
+        cd /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"
+        ls
+        cd /home/mavs/MAVs/results
+        rm /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"/control.csv
+        rm /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"/state.csv
+        rm /home/mavs/MAVs/results/$FOLDERNAME/test"$idx"/nloptcontrol_plannercontrol.csv
+        echo "Exiting: postProcess"
+        idx=$(( $idx+1 ))
+      done
     done
   done
 done
+
+# NOTE for some reason, when the vehicle crashes, only the state is saved in the bag file
